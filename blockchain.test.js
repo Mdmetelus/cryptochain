16663edc38811ccee4e4a3f7097d7523db9dca96
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-20 18:29:47 -0500

    update

--- blockchain.test.js
+++ blockchain.test.js
@@ -4,6 +4,10 @@ const Block = require('./block');
 describe('Blockchain', () => {
     const blockchain = new Blockchain();
 
+    beforeEach(() => {
+        let blockchain = new Blockchain();
+    })
+
     it('contains a `chain` Array instance', () => {
         expect(blockchain.chain instanceof Array).toBe(true);
     });
@@ -17,5 +21,44 @@ describe('Blockchain', () => {
         blockchain.addBlock({ data: newData});
 
         expect(blockchain.chain[blockchain.chain.length -1].data).toEqual(newData);
+    });
+
+    describe('isValidChain()', ()=> {
+        describe('when the chain does not start with the genesis block', () => {
+            it('returns false', ()=> { blockchain.chain[0] = { data: 'fake-genesis'};
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe('when the chain starts with the genesis block and has multiple blocks', ( )=> {
+            describe('and a lastHash reference has changed', ()=> {
+                it ('returns false', () => { 
+                    blockchain.addBlock({ data: 'Lions'});
+                    blockchain.addBlock({ data: 'Tigers'});
+                    blockchain.addBlock({ data: 'Bears'});
+                    blockchain.addBlock({ data: 'Oh-My'});
+                    
+                    blockchain.chain[3].lastHash = 'broken-bruckup-lastHash';
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+            describe('and the chain contains a block with an invalid field', () => {
+                it('returns false', () => { 
+                    blockchain.addBlock({ data: 'Lions'});
+                    blockchain.addBlock({ data: 'Tigers'});
+                    blockchain.addBlock({ data: 'Bears'});
+                    blockchain.addBlock({ data: 'Oh-My'});
+                    
+                    blockchain.chain[3].lastHash = 'bruckup-some-bad-evil-data';
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe('and the chain does not contain any invalid blocks', () => {
+                it('returns true', () => { });
+            });
+        });
     });
 });
