df649a62b698022f7f97abbe86365db501b7555d
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-23 02:36:38 -0500

    "..."

--- pubsub.js
+++ pubsub.js
@@ -1,6 +1,6 @@
 const PubNub = require('pubnub');
 
-const credentials ={
+const credentials = {
     publishKey: 'pub-c-031effba-ffb0-42ac-9f91-7e6808586a57',
     subscribeKey: 'sub-c-dd5386cc-0662-11e9-849f-127435af060e',
     secretKey: 'sec-c-NGUxMTljZTUtYzQ1OC00YWJmLWIxOGUtM2NhNzJjNWJkYTJj'
@@ -8,60 +8,65 @@ const credentials ={
 
 const CHANNELS = {
     TEST: 'TEST',
-    // BLOCKCHAIN: 'BLOCKCHAIN',
-    // TRANSACTION: 'TRANSACTION'
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION: 'TRANSACTION'
 };
 
 class PubSub {
-    constructor({
-        // blockchain, transactionPool
-    }) {
+    constructor({blockchain, transactionPool}) {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
 
-        // this.blockchain = blockchain;
-        // this.transactionPool = transactionPool;
         this.pubnub = new PubNub(credentials);
 
 
         this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
-        // this.pubnub.subscribe({  })
+        
 
         this.pubnub.addListener(this.listener());
+    };
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
     }
 
-    // broadcaastChain() {
-    //     this.publish({
-    //         channel: CHANNELS.BLOCKCHAIN,
-    //         message: JSON.stringify(this.blockchain.chain)
-    //     });
-    // }
-
-    // subscribeToChannels() {
-    //     this.pubnub.subscribe({
-    //         channels: [Object.values(CHANNELS)]
-    //     });
-    // }
-
-
-
+    subscribeToChannels() {
+        this.pubnub.subscribe({
+            channels: [Object.values(CHANNELS)]
+        });
+    }
 
     listener() {
         return {
             message: messageObject => {
                 const { channel, message } = messageObject;
 
-                console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+                console.log(`Message received, said pubsub listener! 
+            Channel: ${channel}. Message: ${message}`);
             }
         };
-    }
+    };
 
     publish({ channel, message }) {
         this.pubnub.publish({ channel, message });
-    }
+    };
 
-    
+    handleMessage({ channel, message }) {
+        const parsedMessage = JSON.parse(message);
+        if(channel === CHANNELS.BLOCKCHAIN ) {
+            this.blockchain.replaceChain(parsedMessage);
+        }
+
+    };
+
+
 };
 
-const testPubSub = new PubSub();
-testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub!!!'});
+// const testPubSub = new PubSub();
+// testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub!!!'});
+
 
 module.exports = PubSub;
